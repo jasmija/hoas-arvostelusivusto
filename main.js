ef83c5be8648170d8f91506b94e459196245c912
timoholo <timo.holopainen2@metropolia.fi>
2021-04-20 11:47:18 +0300

    main.js var -> const

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function addToList() {
-    var title = document.getElementById("title").value;
+    const title = document.getElementById('title').value;
     console.log(title);
 
-    var li = document.createElement('li');
+    const li = document.createElement('li');
     li.innerHTML = title + "<br>";
 
-    var chat = document.getElementById("chat");
+    const chat = document.getElementById('chat');
     chat.appendChild(li);
 }
 
 function findText() {
-    var input = document.getElementById("search").value;
+    const input = document.getElementById('search').value;
     console.log(input);
     alert("String " + input + " found? " + window.find(input));
 }
