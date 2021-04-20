3317354737a4cb6341a7f5d8e5d57835a4a2b0f7
jasmija <jasmin.janka@metropolia.fi>
2021-04-20 09:42:13 +0300

    Testing

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ function addToList() {
     var chat = document.getElementById("chat");
     chat.appendChild(li);
 }
+
+findString = function findText() {
+    var input = document.getElementById("search").value;
+    console.log(input);
+    alert("String \x22" + input + "\x22 found? " + window.find(input));
+    input.scrollIntoView();
+}
+
+function text(value){
+    var div = document.getElementById("display");
+    var text = "";
+
+    if (value == 1) text += "this is one";
+    if (value == 2) text += "this is two";
+    if (value == 3) text += "this is tree";
+
+    div.innerHTML = text;
+}
+
+
