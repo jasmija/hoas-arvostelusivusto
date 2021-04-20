4ad856b06cf7c190b26b38a81abd559dac96065c
jasmija <jasmin.janka@metropolia.fi>
2021-04-20 10:49:38 +0300

    Texts in Finnish

--- main.js
+++ main.js
@@ -9,22 +9,11 @@ function addToList() {
     chat.appendChild(li);
 }
 
-findString = function findText() {
+function findText() {
     var input = document.getElementById("search").value;
     console.log(input);
-    alert("String \x22" + input + "\x22 found? " + window.find(input));
-    input.scrollIntoView();
+    alert("String " + input + " found? " + window.find(input));
 }
 
-function text(value){
-    var div = document.getElementById("display");
-    var text = "";
-
-    if (value == 1) text += "this is one";
-    if (value == 2) text += "this is two";
-    if (value == 3) text += "this is tree";
-
-    div.innerHTML = text;
-}
 
 
