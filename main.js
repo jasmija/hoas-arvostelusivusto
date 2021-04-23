16749f70b2afe9fba3d63c800251b3b757c187f2
timoholo <timo.holopainen2@metropolia.fi>
2021-04-23 15:47:45 +0300

    major update

--- main.js
+++ main.js
@@ -1,176 +1,230 @@
-function addNewChat() {
-    const title = document.getElementById('chatTitle').value;
-    console.log(title);
+function addToList() {
+  const title = document.getElementById('title').value;
+  console.log(title);
 
-    const li = document.createElement('li');
-    li.innerHTML = title + "<br>";
+  const li = document.createElement('li');
+  li.innerHTML = title + '<br>';
 
-    const chat = document.getElementById('chat');
-    chat.appendChild(li);
+  const chat = document.getElementById('chat');
+  chat.appendChild(li);
 }
 
-function openChat() {
-    var ischatopen = document.getElementById('chatcontents').style.visibility;
-    console.log(ischatopen);
-
-    if (ischatopen === "visible") {
-        document.getElementById('chatcontents').style.visibility = "hidden";
-        document.getElementById('chatcontents').style.display = "none";
-    } else {
-        document.getElementById('chatcontents').style.visibility = "visible";
-        document.getElementById('chatcontents').style.display = "block";
-    }
-}
-
-function searchApartment() {
-    var input, uppercase, ul, li, h3, i, text;
-
-    //Get search input field
-    input = document.getElementById("search");
-
-    //Input value to uppercase
-    uppercase = input.value.toUpperCase();
-
-    //List of apartments
-    ul = document.getElementById("apartments");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        h3 = li[i].getElementsByTagName("h3")[0];
-        text = h3.innerText;
-        if (text.toUpperCase().indexOf(uppercase) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-
-window.onload = function(){
-//VIEW REVIEWS
-//Get the modal
-var modal = document.getElementById("modal");
-
-//Get the button that opens the modal
-var btn = document.getElementById("ratings");
-
-//Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-//Open the modal, when the user clicks the image
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-//Close the modal, when the user clicks on <span> (x)
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-//Close modal, when the user clicks anywhere outside of the modal
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-//ADD REVIEW
-    var modal2 = document.getElementById("modal2");
-    console.log("after get modal");
-
-    var ratebuttom = document.getElementById("kimpitie");
-    console.log("after get arvostele button");
-
-    var span2 = document.getElementsByClassName("close2")[0];
-    console.log("after get close");
-
-    ratebuttom.onclick = function() {
-        console.log("open modal when user clicks arvostelebutton");
-        modal2.style.display = "block";
-    }
-
-    span2.onclick = function() {
-        modal2.style.display = "none";
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal2) {
-            modal2.style.display = "none";
-        }
-    }
-};
-
-function visibleLogin() {
-    var isloginopen = document.getElementById('login_wrap').style.visibility;
-    console.log(isloginopen);
-    if(isloginopen === "visible"){
-        document.getElementById('login_wrap').style.visibility = "hidden";
-        document.getElementById('login_wrap').style.display = "none";
-    }else{
-        document.getElementById("login_wrap").style.visibility = "visible";
-        document.getElementById('login_wrap').style.display = "block";
-    }
-};
-
-function hideLogin() {
-    document.getElementById("login_wrap").style.visibility = "hidden";
-};
-
-/*CTRL F SEARCH FROM PAGE*/
 /*function findText() {
     const input = document.getElementById('search').value;
     console.log(input);
     alert("String " + input + " found? " + window.find(input));
 }*/
 
-//SERVER JS
-var json; //json is global...
+function searchApartment() {
+  let input, uppercase, ul, li, h3, i, text;
 
-function makeQuery() {
-    var id = document.getElementById("searchid").value;
-    if (id.length == 0) { // fix this and support empty field
-        return;
+  //Get search input field
+  input = document.getElementById('search');
+
+  //Input value to uppercase
+  uppercase = input.value.toUpperCase();
+
+  //List of apartments
+  ul = document.getElementById('apartments');
+  li = ul.getElementsByTagName('li');
+  for (i = 0; i < li.length; i++) {
+    h3 = li[i].getElementsByTagName('h3')[0];
+    text = h3.innerText;
+    if (text.toUpperCase().indexOf(uppercase) > -1) {
+      li[i].style.display = '';
     } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                json = JSON.parse(xmlhttp.responseText);
-                console.log(json);
-                //myFunction(resultArr);
-                //document.getElementById("info").innerHTML = xmlhttp.responseText;
-                if (json.length> 0){ // something found
-                    //console.log(json.length + ", " + json.rows[0].Name);
-                    showList(json);
-                }
-                else {
-                    document.getElementById("rating").innerHTML = "<br/>Arvosteluita ei löytynyt yhtään.";
-                }
-            }
-        };
-        var searchedid = id;
-        console.log("Haettu id: " + searchedid);
-        console.log("http://localhost:8084/api/results?id=" + searchedid);
-        xmlhttp.open("GET", "http://localhost:8084/api/results?id=" + searchedid, true);
-        xmlhttp.send();
+      li[i].style.display = 'none';
     }
+  }
 }
 
-function showList(json) {
-    console.log("showList");
+if (typeof window !== 'undefined') {
+  window.onload = function() {
 
-    var divElement = document.getElementById("rating");
+//Get the modal
+    const modal = document.getElementById('modal');
 
-    var i;
-    var string;
+//Get the button that opens the modal
+    const btn = document.getElementById('ratings');
+
+//Get the <span> element that closes the modal
+    const span = document.getElementsByClassName('close')[0];
+
+//Open the modal, when the user clicks the kirjaudu button
+    btn.onclick = function() {
+      modal.style.display = 'block';
+    };
+
+//Close the modal, when the user clicks on <span> (x)
+    span.onclick = function() {
+      modal.style.display = 'none';
+    };
+
+//Close modal, when the user clicks anywhere outside of the modal
+    window.onclick = function(event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    };
+  };
+
+  // Main page "log in pop-up" functions
+  function visibleLogin() {
+    const isloginopen = document.getElementById('login_wrap').style.visibility;
+    console.log(isloginopen);
+    if (isloginopen === 'visible') {
+      document.getElementById('login_wrap').style.visibility = 'hidden';
+      document.getElementById('login_wrap').style.display = 'none';
+    } else {
+      document.getElementById('login_wrap').style.visibility = 'visible';
+      document.getElementById('login_wrap').style.display = 'block';
+    }
+  }
+
+  function hideLogin() {
+    document.getElementById('login_wrap').style.visibility = 'hidden';
+  }
+
+  /*CTRL F SEARCH FROM PAGE*/
+  /*function findText() {
+      const input = document.getElementById('search').value;
+      console.log(input);
+      alert("String " + input + " found? " + window.find(input));
+  }*/
+
+//SERVER JS
+  let json; //json is global...
+
+  function makeQuery() {
+    const id = document.getElementById('searchid').value;
+    if (id.length === 0) { // fix this and support empty field
+    } else {
+      const xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+          json = JSON.parse(xmlhttp.responseText);
+          console.log(json);
+          //myFunction(resultArr);
+          //document.getElementById("info").innerHTML = xmlhttp.responseText;
+          if (json.length > 0) { // something found
+            //console.log(json.length + ", " + json.rows[0].Name);
+            showList(json);
+          } else {
+            document.getElementById(
+                'rating').innerHTML = '<br/>Arvosteluita ei löytynyt yhtään.';
+          }
+        }
+      };
+      const searchedid = id;
+      console.log('Haettu id: ' + searchedid);
+      console.log('http://localhost:8084/api/results?id=' + searchedid);
+      xmlhttp.open('GET', 'http://localhost:8084/api/results?id=' + searchedid,
+          true);
+      xmlhttp.send();
+    }
+  }
+
+  function showList(json) {
+    console.log('showList');
+
+    const divElement = document.getElementById('rating');
+
+    let i;
+    let string;
 
     //var inputvalue = document.getElementsByTagName("input").value;
     //console.log("inputavalue " + inputvalue);
     for (i in json) {
-        string = json[i].id + ", " + json[i].osoite + ", "+json[i].kunto +", "+json[i].viihtyvyys+", "+json[i].kokonaisarvosana + ", " + json[i].vapaasana;
-        divElement.innerHTML = string;
-        console.log(string);
+      string = json[i].id + ', ' + json[i].osoite + ', ' + json[i].kunto +
+          ', ' + json[i].viihtyvyys + ', ' + json[i].kokonaisarvosana + ', ' +
+          json[i].vapaasana;
+      divElement.innerHTML = string;
+      console.log(string);
     }
+  }
 }
 
+//*** BELOW ONLY CONNECTION TO DATABASE AND REST RELATED STUFF ***//
 
+// Modules to use
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+// Add database connection details to variable "connection"
+const connection = mysql.createConnection({
+  host: 'mysql.metropolia.fi',
+  user: 'timoholo',
+  password: 'A3sC8Qgp9MWiKOb7GPRV',
+  database: 'timoholo',
+});
 
+const app = express();
+
+// Listen to port 3000
+app.listen(3000, () => console.log('Listening at port 3000'));
+
+// Needed for css and images to work
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Send static file "main.html" to client when browsing at "http://localhost:3000/"
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname + '/main.html'));
+});
+
+// Send static file "signup.html" to client when browsing at "http://localhost:3000/register"
+app.get('/register', (request, response) => {
+  response.sendFile(__dirname + '/signup.html');
+});
+
+// Send static file "login.html" to client when browsing at "http://localhost:3000/login"
+app.get('/login', (request, response) => {
+  response.sendFile(__dirname + '/login.html');
+});
+
+// Work in progress
+app.post('/auth', function(request, response) {
+  const username = request.body.username;
+  const password = request.body.password;
+  // If both fields contain something, do if statement where database is checked for the values.
+  if (username && password) {
+    connection.query(
+        'SELECT * FROM accounts WHERE username = ? AND password = ?',
+        [username, password], function(error, results, fields) {
+          if (results.length > 0) {
+            request.session.loggedin = true;
+            request.session.username = username;
+            response.redirect('/home');
+          } else {
+            response.send('Salasana tai käyttäjätunnus väärin!');
+          }
+          response.end();
+        });
+    // If one field is empty, or both, send the following message.
+  } else {
+    response.send(
+        'Käyttäjätunnus tai salasana puuttuu kirjautumiskentästä. Ehkä molemmat? Tarkasta. <br>' +
+        ' (Normaalioloissa tavallisen käyttäjän ei kuuluisi nähdä tätä sivua. Kirjautumiskenttien pitäisi tarkastaa että molemmissa on' +
+        ' edes jotain kirjoitettuna).');
+    response.end();
+  }
+});
+
+app.get('/home', function(request, response) {
+  if (request.session.loggedin) {
+    response.sendFile(__dirname + '/main.html');
+  } else {
+    response.send('Kirjaudu sisään nähdäksesi tämän sivun.');
+  }
+});
