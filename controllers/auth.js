1bbdb8bbe3b5ca7597f981be4de385da9844bb02
timoholo <timo.holopainen2@metropolia.fi>
2021-04-26 17:02:37 +0300

    code cleanup

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,43 +3,44 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const connection = mysql.createConnection({
-  host: "mysql.metropolia.fi",
-  user: "jasmija",
-  password: "jassumetropoliasql",
-  database: "jasmija"
+  host: 'mysql.metropolia.fi',
+  user: 'jasmija',
+  password: 'jassumetropoliasql',
+  database: 'jasmija',
 });
 
 exports.register = (request, response) => {
   console.log(request.body);
 
-  let { username, password, passwordConfirm } = request.body;
+  let {username, password, passwordConfirm} = request.body;
 
   // Check if database already has username or check if password confirmation doesn't match.
-  connection.query('SELECT username FROM accounts WHERE username = ?', [username], async (error, results) => {
-    if(error) {
-      console.log(error);
-    }
-    if(results.length > 0) {
-      console.log('Username taken!')
-      return response.sendFile('signup.html', { root: './'});
-    }
-    else if(password !== passwordConfirm) {
-      console.log('Passwords do not match!')
-      return response.sendFile('signup.html', { root: './'});
-    }
+  connection.query('SELECT username FROM accounts WHERE username = ?',
+      [username], async (error, results) => {
+        if (error) {
+          console.log(error);
+        }
+        if (results.length > 0) {
+          console.log('Username taken!');
+          return response.sendFile('signup.html', {root: './'});
+        } else if (password !== passwordConfirm) {
+          console.log('Passwords do not match!');
+          return response.sendFile('signup.html', {root: './'});
+        }
 
-    // Hash user password with 8 cycles.
-    let hashedPassword = await bcrypt.hash(password, 8);
-    console.log('Hashed password: ' + hashedPassword);
+        // Hash user password with 8 cycles.
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log('Hashed password: ' + hashedPassword);
 
-    connection.query('INSERT INTO accounts SET ?', { username: username, password: hashedPassword },  (error, results) => {
-      if(error) {
-        console.log(error)
-      }
-      else {
-        console.log('User "' + username +  '" created!')
-        return response.sendFile('signup.html', { root: './'});
-      }
-    })
-  })
-}
+        connection.query('INSERT INTO accounts SET ?',
+            {username: username, password: hashedPassword},
+            (error, results) => {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log('User "' + username + '" created!');
+                return response.sendFile('signup.html', {root: './'});
+              }
+            });
+      });
+};
