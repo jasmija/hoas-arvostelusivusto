e0860f09526736dd471bffce01f7ef6aa373bebd
jasmija <jasmin.janka@metropolia.fi>
2021-05-04 22:48:12 +0300

    required

--- js/homepage.js
+++ js/homepage.js
@@ -438,18 +438,18 @@ function makeQueryForSendChat() {
   const newChat = '{"username": "' + user + '", "header": "' + header + '"}';
   console.log('Console loggina ' + newChat);
 
-  const xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-      addNewChat();
-      //document.getElementById('testchat').innerHTML = xmlhttp.responseText;
-    }
-  };
-  console.log('http://localhost:3000/addchat');
-  xmlhttp.open('POST', 'http://localhost:3000/addchat', true);
-  xmlhttp.setRequestHeader('Content-Type', 'application/json');
-  xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-  xmlhttp.send(newChat);
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+        addNewChat();
+        //document.getElementById('testchat').innerHTML = xmlhttp.responseText;
+      }
+    };
+    console.log('http://localhost:3000/addchat');
+    xmlhttp.open('POST', 'http://localhost:3000/addchat', true);
+    xmlhttp.setRequestHeader('Content-Type', 'application/json');
+    xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xmlhttp.send(newChat);
 }
 
 function makeQueryForSendAnswer(id) {
